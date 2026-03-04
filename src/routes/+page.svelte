25731c943af0f66e70b6daa2b4e1d49abf3795e1
Kira <kira.eva@protonmail.com>
2026-03-04 21:33:58 +0000

    ignore layout.js to fix ts temporarily

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-
     import type { Scene } from "phaser";
     import type { MainMenu } from "../game/scenes/MainMenu";
     import PhaserGame, { type TPhaserRef } from "../PhaserGame.svelte";
@@ -8,55 +7,28 @@
     let canMoveSprite = false;
 
     //  References to the PhaserGame component (game and scene are exposed)
-    let phaserRef: TPhaserRef = { game: null, scene: null};
+    let phaserRef: TPhaserRef = { game: null, scene: null };
     const spritePosition = { x: 0, y: 0 };
 
     const changeScene = () => {
-
         const scene = phaserRef.scene as MainMenu;
 
-        if (scene)
-        {
-
+        if (scene) {
             // Call the changeScene method defined in the `MainMenu`, `Game` and `GameOver` Scenes
             scene.changeScene();
-
         }
-
-    }
-    
-    const moveSprite = () => {
-
-        const scene = phaserRef.scene as MainMenu;
-
-        if (scene)
-        {
-
-            // Get the update logo position
-            (scene as MainMenu).moveLogo(({ x, y }) => {
-
-                spritePosition.x = x;
-                spritePosition.y = y;
-
-            });
-
-        }
-
-    }
+    };
 
     const addSprite = () => {
-
         const scene = phaserRef.scene as Scene;
 
-        if (scene)
-        {
-
+        if (scene) {
             // Add more stars
             const x = Phaser.Math.Between(64, scene.scale.width - 64);
             const y = Phaser.Math.Between(64, scene.scale.height - 64);
 
             //  `add.sprite` is a Phaser GameObjectFactory method and it returns a Sprite Game Object instance
-            const star = scene.add.sprite(x, y, 'star');
+            const star = scene.add.sprite(x, y, "star");
 
             //  ... which you can then act upon. Here we create a Phaser Tween to fade the star sprite in and out.
             //  You could, of course, do this from within the Phaser Scene code, but this is just an example
@@ -66,30 +38,22 @@
                 duration: 500 + Math.random() * 1000,
                 alpha: 0,
                 yoyo: true,
-                repeat: -1
+                repeat: -1,
             });
-            
         }
-
-    }
+    };
 
     // Event emitted from the PhaserGame component
     const currentScene = (scene: Scene) => {
-
-        canMoveSprite = (scene.scene.key !== "MainMenu");
-
-    }
-    
+        canMoveSprite = scene.scene.key !== "MainMenu";
+    };
 </script>
 
 <div id="app">
-    <PhaserGame bind:phaserRef={phaserRef} currentActiveScene={currentScene} />
+    <PhaserGame bind:phaserRef currentActiveScene={currentScene} />
     <div>
         <div>
             <button class="button" on:click={changeScene}>Change Scene</button>
-        </div>
-        <div>
-            <button class="button" disabled={canMoveSprite} on:click={moveSprite}>Toggle Movement</button>
         </div>
         <div class="spritePosition">
             Sprite Position:
@@ -110,7 +74,7 @@
         justify-content: center;
         align-items: center;
     }
-    
+
     .spritePosition {
         margin: 10px 0 0 10px;
         font-size: 0.8em;
